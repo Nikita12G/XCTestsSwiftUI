ea3ff0ec0b1e81cdb4f3e8a064d50b3d68816bf2
Nikita12G <gulyi08@mail.ru>
2024-08-19 19:29:05 +0700

    Initial Commit

---
+++ XCTestsSwiftUI/XCTestsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCTestsSwiftUIApp.swift
+//  XCTestsSwiftUI
+//
+//  Created by Никита Гуляев on 19.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct XCTestsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
